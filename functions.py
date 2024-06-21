093c19c49a6168fa7a55e7e822c475dbd498f711
a <fen.adelina@gmail.com>
2024-06-21 23:49:20 +0300

    Initial commit

---
+++ functions.py
@@ -0,0 +1,20 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):  # default argument
+    """ Read a text file and return the list of to-do items """
+    with open(filepath, "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to-do items list in the text file """
+    with open(filepath, "w") as file_local:
+        file_local.writelines(todos_arg)
+    # more like a procedure, does not return anything
+
+
+if __name__ == "__main__":  # variable defined hiddenly by python
+    print("hello from functions")
+    print(get_todos())
